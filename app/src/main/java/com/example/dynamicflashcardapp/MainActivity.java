5d22d0e0dbb56e46b78a07ca780c98477d31e390
Ikraam Rahman <reachikraam@gmail.com>
2022-03-18 18:24:00 -0500

    Additional features added to app

--- app/src/main/java/com/example/dynamicflashcardapp/MainActivity.java
+++ app/src/main/java/com/example/dynamicflashcardapp/MainActivity.java
@@ -1,30 +1,61 @@
 package com.example.dynamicflashcardapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.provider.Telephony;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView questionTextView;
+    TextView answerTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView flashcardQuestion = findViewById(R.id.flashcard_question);
-        TextView flashcardAnswer = findViewById(R.id.flashcard_answer);
-        flashcardQuestion.setOnClickListener(new View.OnClickListener() {
+        questionTextView = findViewById(R.id.flashcard_question);
+        answerTextView = findViewById(R.id.flashcard_answer);
+        questionTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                flashcardQuestion.setVisibility(View.INVISIBLE);
-                flashcardAnswer.setVisibility(View.VISIBLE);
+                questionTextView.setVisibility(View.INVISIBLE);
+                answerTextView.setVisibility(View.VISIBLE);
             }
         });
+
+        ImageView addQuestionImageView = findViewById(R.id.flashcard_add_question_button);
+        addQuestionImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, AddCardActivity.class);
+               // startActivity(intent);
+                startActivityForResult(intent, 100);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 100 && resultCode == RESULT_OK) {
+            if (data!= null) {
+                String questionString = data.getExtras().getString("QUESTION_KEY");
+                String answerString = data.getExtras().getString("ANSWER_KEY");
+                questionTextView.setText(questionString);
+                answerTextView.setText(answerString);
+            }
+        }
+
     }
 }
