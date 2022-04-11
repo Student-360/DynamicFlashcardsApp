49e3e4791e65923e4f0ddff530659f3a50f85f05
Ikraam Rahman <reachikraam@gmail.com>
2022-04-10 23:49:33 -0500

    Transitions created and implemented

--- app/src/main/java/com/example/dynamicflashcardapp/MainActivity.java
+++ app/src/main/java/com/example/dynamicflashcardapp/MainActivity.java
@@ -3,13 +3,18 @@ package com.example.dynamicflashcardapp;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.Animator;
 import android.content.Intent;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.provider.Telephony;
 import android.view.View;
+import android.view.ViewAnimationUtils;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -40,6 +45,25 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 questionTextView.setVisibility(View.INVISIBLE);
                 answerTextView.setVisibility(View.VISIBLE);
+
+                View answerTextView = findViewById(R.id.flashcard_answer);
+
+// get the center for the clipping circle
+                int cx = answerTextView.getWidth() / 2;
+                int cy = answerTextView.getHeight() / 2;
+
+// get the final radius for the clipping circle
+                float finalRadius = (float) Math.hypot(cx, cy);
+
+// create the animator for this view (the start radius is zero)
+                Animator anim = ViewAnimationUtils.createCircularReveal(answerTextView, cx, cy, 0f, finalRadius);
+
+// hide the question and show the answer to prepare for playing the animation!
+                questionTextView.setVisibility(View.INVISIBLE);
+                answerTextView.setVisibility(View.VISIBLE);
+
+                anim.setDuration(3000);
+                anim.start();
             }
         });
 
@@ -80,6 +104,28 @@ public class MainActivity extends AppCompatActivity {
                 questionTextView.setVisibility(View.VISIBLE);
                 answerTextView.setVisibility(View.INVISIBLE);
 
+
+
+                final Animation leftOutAnim = AnimationUtils.loadAnimation(view.getContext(), R.anim.left_out);
+                final Animation rightInAnim = AnimationUtils.loadAnimation(view.getContext(), R.anim.right_in);
+
+                leftOutAnim.setAnimationListener(new Animation.AnimationListener() {
+                    @Override
+                    public void onAnimationStart(Animation animation) {
+                        // this method is called when the animation first starts
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animation animation) {
+                        // this method is called when the animation is finished playing
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animation animation) {
+                        // we don't need to worry about this method
+                    }
+                });
+
                 if(cardIndex >= allFlashcards.size()) {
                     Snackbar.make(view,
                             "You've reached the end of the cards, going back to start.",
@@ -99,18 +145,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == 100 && resultCode == RESULT_OK) {
-            if (data!= null) {
+            if (data != null) {
                 String questionString = data.getExtras().getString("QUESTION_KEY");
                 String answerString = data.getExtras().getString("ANSWER_KEY");
                 questionTextView.setText(questionString);
                 answerTextView.setText(answerString);
 
-               Flashcard flashcard = new Flashcard(questionString, answerString);
-               flashcardDatabase.insertCard(flashcard);
+                Flashcard flashcard = new Flashcard(questionString, answerString);
+                flashcardDatabase.insertCard(flashcard);
 
-               allFlashcards = flashcardDatabase.getAllCards();
+                allFlashcards = flashcardDatabase.getAllCards();
             }
+
         }
 
     }
-}
+
+
+    }
+
+
+
+
+
+
